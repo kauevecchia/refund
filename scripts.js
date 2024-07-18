fb3e83e113f5df997f0008f86cf1fb7d0ab2675b
Kauê <kauevecchia@hotmail.com>
2024-07-17 22:11:13 -0300

    Obtendo os dados do formulário

--- scripts.js
+++ scripts.js
@@ -1,8 +1,31 @@
 // Seleciona os elemtentos do formulário.
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
+// Captura o evento de input para formatar o valor.
 amount.oninput = () => {
+    // Obtém o valor atual do input e remove os caracteres não numéricos.
     let value = amount.value.replace(/\D/g, "")
 
-    amount.value = value
+    // Transforma o valor em centavos (Exemplo: 150/100 = 1.5 que é equivalente a R$1,50).
+    value = Number(value) / 100
+
+    // Atualiza o valor do input.
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    // Formata o valor no padrão BRL (real Brasileiro).
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    return value
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault()
 }
